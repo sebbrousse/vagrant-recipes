42534bd43aa1415b6d75779d55dddbe6a743b19b
Sebastien Brousse <seb.brousse@gmail.com>
2014-04-09 15:07:12 +0200

    ajout de l'installation de Java pour ES

--- elasticsearch/elasticsearch.sh
+++ elasticsearch/elasticsearch.sh
@@ -9,6 +9,10 @@ echo Installing elasticsearch repository
 echo Install APT Packages
 	apt-get update
 
+echo Install Java JDK
+	apt-get -y install openjdk-7-jdk
+	update-java-alternatives -s java-1.7.0-openjdk-amd64	
+
 echo Install Elasticsearch
 	apt-get -y install elasticsearch=$ES_VERSION
 	update-rc.d elasticsearch defaults
